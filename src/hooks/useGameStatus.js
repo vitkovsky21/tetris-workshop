2e6efdb2449a4726406bf4b914a69dcd58539d86
vitkovsky21 <vladbekkoshkarov@yandex.ru>
2022-09-29 16:48:49 +0300

    Tetris created

---
+++ src/hooks/useGameStatus.js
@@ -0,0 +1,23 @@
+import { useState, useEffect, useCallback } from 'react';
+
+export const useGameStatus = rowsCleared => {
+    const [score, setScore] = useState(0);
+    const [rows, setRows] = useState(0);
+    const [level, setLevel] = useState(0);
+
+    const linePoints = [40, 100, 300, 1200];
+
+    const calcScore = useCallback(() => {
+        if (rowsCleared > 0) {
+            setScore(prev => prev + linePoints[rowsCleared / 2 - 1] * (level + 1));
+            setRows(prev => prev + rowsCleared / 2);
+        }
+    }, [level, linePoints, rowsCleared]);
+
+    useEffect(() => {
+        console.log(rows, level, score)
+        calcScore();
+    }, [calcScore, rowsCleared, score]);
+
+    return [score, setScore, rows, setRows, level, setLevel]
+}
